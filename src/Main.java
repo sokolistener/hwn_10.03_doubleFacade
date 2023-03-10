0cf1aba32643afa16cc70c4df325b1537a08738b
sokolistener <holmse113@mail.ru>
2023-03-10 21:03:10 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1","11"));
+        System.out.println(bins.mult("100","10"));
+    }
+}
